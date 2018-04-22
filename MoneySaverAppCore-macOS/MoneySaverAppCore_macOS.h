736acd2802acf205690f671e24fbd64b1203c420
Michał Moskała <moskala.michal@gmail.com>
2018-04-22 17:48:43 +0200

    Added macOS target

---
+++ MoneySaverAppCore-macOS/MoneySaverAppCore_macOS.h
@@ -0,0 +1,18 @@
+//
+//  MoneySaverAppCore_macOS.h
+//  MoneySaverAppCore-macOS
+//
+//  Created by Michal Moskala on 22.04.2018.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MoneySaverAppCore_macOS.
+FOUNDATION_EXPORT double MoneySaverAppCore_macOSVersionNumber;
+
+//! Project version string for MoneySaverAppCore_macOS.
+FOUNDATION_EXPORT const unsigned char MoneySaverAppCore_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoneySaverAppCore_macOS/PublicHeader.h>
+
+
