ee69e520abfcb76d786cfa586cae709aa2a2493a
Michał Moskała <moskala.michal@gmail.com>
2018-04-22 18:03:57 +0200

    Cleaned up project

---
+++ Sources/MoneySaverAppCore.h
@@ -0,0 +1,11 @@
+//
+//  MoneySaverAppCore_iOS.h
+//  MoneySaverAppCore-iOS
+//
+//  Created by Michal Moskala on 22.04.2018.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double MoneySaverAppCore_VersionNumber;
+FOUNDATION_EXPORT const unsigned char MoneySaverAppCore_VersionString[];
