3116373d4e4609a2f1e04dc2086f5764af1985e2
Michał Moskała <moskala.michal@gmail.com>
2018-04-23 21:17:47 +0200

    Expose save method of CoreDataStack

--- Sources/CoreData/CoreDataStack.swift
+++ Sources/CoreData/CoreDataStack.swift
@@ -11,55 +11,25 @@ import CoreData
 
 protocol CoreDataStack: class {
     func getViewContext() -> NSManagedObjectContext
+    func save() throws
 }
 
 class CoreDataStackImplementation: CoreDataStack {
     
     private let persistentContainer: NSPersistentContainer
-    private let notificationCenter: NotificationCenter
-    private var didEnterBackgroundToken: NSObjectProtocol?
-    private var willTerminateToken: NSObjectProtocol?
     
-    init(notificationCenter: NotificationCenter) {
-        self.notificationCenter = notificationCenter
+    init() {
         self.persistentContainer = NSPersistentContainer(name: "DataModel")
         self.persistentContainer.loadPersistentStores(completionHandler: { (_, _) in })
-        registerForNotifications()
-    }
-    
-    private func registerForNotifications() {
-//        didEnterBackgroundToken = notificationCenter.addObserver(forName: .UIApplicationDidEnterBackground,
-//                                                                 object: nil,
-//                                                                 queue: .main, using: { [weak self] _ in
-//            self?.save()
-//        })
-//        
-//        willTerminateToken = notificationCenter.addObserver(forName: .UIApplicationWillTerminate,
-//                                                            object: nil,
-//                                                            queue: .main,
-//                                                            using: { [weak self] _ in
-//            self?.save()
-//        })
     }
     
     func getViewContext() -> NSManagedObjectContext {
         return persistentContainer.viewContext
     }
     
-    private func save() {
-        do {
-            try getViewContext().save()
-        } catch {
-
-        }
-    }
-    
-    deinit {
-        if let token = didEnterBackgroundToken {
-            notificationCenter.removeObserver(token)
-        }
-        if let token = willTerminateToken {
-            notificationCenter.removeObserver(token)
-        }
+    func save() throws {
+        let context = persistentContainer.viewContext
+        guard context.hasChanges else { return }
+        try context.save()
     }
 }
