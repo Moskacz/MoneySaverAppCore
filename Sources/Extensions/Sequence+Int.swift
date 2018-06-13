0455d6bb9779513e8b9b7cf9da7fde425af843d5
Michał Moskała <moskala.michal@gmail.com>
2018-06-13 22:03:59 +0200

    Created new file with extensions for Sequence<Int> alongside with unit tests

---
+++ Sources/Extensions/Sequence+Int.swift
@@ -0,0 +1,16 @@
+//
+//  Sequence+Int.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 13.06.2018.
+//
+
+import Foundation
+
+extension Sequence where Element == Int {
+    
+    var rangesOfMissingValues: [Range<Int>] {
+        return []
+    }
+    
+}
---
+++ UnitTests/SequenceIntExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  SequenceIntExtensions.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 13.06.2018.
+//
+
+import XCTest
+import MoneySaverAppCore
+
+class SequenceIntExtensions: XCTestCase {
+    
+    func test_rangeOfMissingValues() {
+        
+    }
+    
+}
