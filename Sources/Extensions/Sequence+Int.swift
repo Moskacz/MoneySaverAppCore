fcca8dc6ffdebdb1886a6d661b5e21f0e9c184e8
Michał Moskała <moskala.michal@gmail.com>
2018-06-14 18:28:49 +0200

    Computing ranges of missing values on sequence

--- Sources/Extensions/Sequence+Int.swift
+++ Sources/Extensions/Sequence+Int.swift
@@ -9,8 +9,24 @@ import Foundation
 
 extension Sequence where Element == Int {
     
-    var rangesOfMissingValues: [Range<Int>] {
-        return []
+    public var rangesOfMissingValues: [CountableClosedRange<Int>] {
+        let sortedElements = sorted()
+        var iterator = sortedElements.makeIterator()
+        guard var value = iterator.next() else { return [] }
+        
+        var ranges = [CountableClosedRange<Int>]()
+        
+        while let nextValue = iterator.next() {
+            if value.distance(to: nextValue) > 1 {
+                let rangeStart = value + 1
+                let rangeEnd = nextValue - 1
+                let range = rangeStart...rangeEnd;
+                ranges.append(range)
+            }
+            value = nextValue
+        }
+        
+        return ranges
     }
     
 }
--- UnitTests/SequenceIntExtensions.swift
+++ UnitTests/SequenceIntExtensions.swift
@@ -11,7 +11,17 @@ import MoneySaverAppCore
 class SequenceIntExtensions: XCTestCase {
     
     func test_rangeOfMissingValues() {
-        
+        let values = [0, 9]
+        let expectedMissingRange = 1...8
+        let computedRanges = values.rangesOfMissingValues
+        XCTAssertEqual(computedRanges.count, 1)
+        XCTAssertEqual(computedRanges[0], expectedMissingRange)
+    }
+    
+    func test_rangeOfMissingValues_multipleGaps() {
+        let values = [0, 9, 13]
+        let expectedRanges = [1...8, 10...12]
+        XCTAssertEqual(values.rangesOfMissingValues, expectedRanges)
     }
     
 }
