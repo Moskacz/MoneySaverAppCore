d454912b3215c1cd7a226e03909bc6df41ad4eca
Michał Moskała <moskala.michal@gmail.com>
2018-06-16 12:34:05 +0200

    Implemented StatsChartsDataProcessor by FakeChartsDataProcessor

--- UnitTests/Fakes/FakeChartsDataProcessor.swift
+++ UnitTests/Fakes/FakeChartsDataProcessor.swift
@@ -8,15 +8,33 @@
 
 import Foundation
 
-public class FakeChartsDataProcessor: BudgetChartsDataProcessor {
+public class FakeChartsDataProcessor {
     
     public init() {}
+}
+
+extension FakeChartsDataProcessor: BudgetChartsDataProcessor {
     
     public func spendings(fromMonthlyExpenses expenses: [DatedValue]) -> [PlotValue] {
         fatalError()
     }
     
     public func estimatedSpendings(budgetValue: Double) -> [PlotValue] {
+        fatalError()
+    }
+}
+
+extension FakeChartsDataProcessor: StatsChartsDataProcessor {
+    
+    public func expensesGroupedBy<T>(grouping: TransactionsGrouping, transactions: [T]) -> [PlotValue] where T : TransactionProtocol {
+        fatalError()
+    }
+    
+    public func incomesGroupedBy<T>(grouping: TransactionsGrouping, transactions: [T]) -> [PlotValue] where T : TransactionProtocol {
+        fatalError()
+    }
+    
+    public func expensesGroupedByCategories<T>(_ transactions: [T]) -> [CategorySum] where T : TransactionProtocol {
         fatalError()
     }
 }
