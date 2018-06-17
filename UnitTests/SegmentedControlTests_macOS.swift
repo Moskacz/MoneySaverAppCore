9df42bec90c880f04d8cc22410204065f6fe5597
Michał Moskała <moskala.michal@gmail.com>
2018-06-17 21:49:42 +0200

    Additional unit tests for NSSegmentedControl

--- UnitTests/SegmentedControlTests_macOS.swift
+++ UnitTests/SegmentedControlTests_macOS.swift
@@ -11,26 +11,23 @@ import MoneySaverAppCore
 
 class SegmentedControlTests: XCTestCase {
     
-    var sut: NSSegmentedControl!
-    
-    override func setUp() {
-        super.setUp()
-        sut = NSSegmentedControl(labels: ["a", "b", "c"], trackingMode: .selectOne, target: nil, action: nil)
-    }
-    
-    override func tearDown() {
-        sut = nil
-        super.tearDown()
-    }
-    
     func test_setItems() {
+        let sut = NSSegmentedControl()
         sut.items = [SegmentedControlItem.text("label1"), SegmentedControlItem.text("label2")]
         XCTAssertEqual(sut.segmentCount, 2)
         XCTAssertEqual(sut.label(forSegment: 0), "label1")
         XCTAssertEqual(sut.label(forSegment: 1), "label2")
     }
     
+    func test_setItems_images() {
+        let sut = NSSegmentedControl()
+        sut.items = [SegmentedControlItem.image(NSImage()), SegmentedControlItem.image(NSImage())]
+        XCTAssertNotNil(sut.image(forSegment: 0))
+        XCTAssertNotNil(sut.image(forSegment: 1))
+    }
+    
     func test_getItems() {
+        let sut = NSSegmentedControl(labels: ["a", "b", "c"], trackingMode: .selectOne, target: nil, action: nil)
         let items = sut.items
         XCTAssertEqual(items.count, 3)
         XCTAssertEqual(items[0], SegmentedControlItem.text("a"))
@@ -38,4 +35,8 @@ class SegmentedControlTests: XCTestCase {
         XCTAssertEqual(items[2], SegmentedControlItem.text("c"))
     }
     
+    func test_getItems_images() {
+        let sut = NSSegmentedControl(images: [NSImage(), NSImage()], trackingMode: .selectOne, target: nil, action: nil)
+        XCTAssertEqual(sut.items.count, 2)
+    }
 }
