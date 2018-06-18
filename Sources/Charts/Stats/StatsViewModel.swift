f9907b2c4e5b4a78caadc63ba80d8e6611b7e4d1
Michał Moskała <moskala.michal@gmail.com>
2018-06-18 18:41:52 +0200

    StatsViewModel added

---
+++ Sources/Charts/Stats/StatsViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  StatsViewModel.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 18.06.2018.
+//
+
+import Foundation
+
+public protocol StatsViewModelDelegate: class {
+    func dataUpdated()
+}
+
+public class StatsViewModel {
+    
+    private let repository: TransactionsRepository
+    public weak var delegate: StatsViewModelDelegate?
+    
+    public init(repository: TransactionsRepository) {
+        self.repository = repository
+    }
+}
