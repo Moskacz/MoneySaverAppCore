2d960cafb894938c34d2bced9482ea61bf540972
Michał Moskała <moskala.michal@gmail.com>
2018-06-21 22:18:06 +0200

    Added CoreDataNotifications protocol

--- Sources/Extensions/NotificationCenter+Extensions.swift
+++ Sources/Extensions/NotificationCenter+Extensions.swift
@@ -6,4 +6,20 @@
 //
 
 import Foundation
+import CoreData
 
+protocol CoreDataNotifications {
+    func observeObjectsDidChange(context: NSManagedObjectContext,
+                                 callback: @escaping (Notification) -> Void) -> NSObjectProtocol
+}
+
+extension NotificationCenter: CoreDataNotifications {
+    
+    func observeObjectsDidChange(context: NSManagedObjectContext,
+                                 callback: @escaping (Notification) -> Void) -> NSObjectProtocol {
+        let notificationName = Notification.Name.NSManagedObjectContextObjectsDidChange
+        return addObserver(forName: notificationName, object: context, queue: OperationQueue.main, using: { notification in
+            callback(notification)
+        })
+    }
+}
