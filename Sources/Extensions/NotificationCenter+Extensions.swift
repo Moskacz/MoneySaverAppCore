094a45392b117018fe56b7ac35e61d0e67796ac6
Michał Moskała <moskala.michal@gmail.com>
2018-06-23 12:42:05 +0200

    Added TransactionNotifications protocol and implementation for NotificaionCenter

--- Sources/Extensions/NotificationCenter+Extensions.swift
+++ Sources/Extensions/NotificationCenter+Extensions.swift
@@ -24,3 +24,53 @@ extension NotificationCenter: CoreDataNotifications {
         return ObservationToken(notificationCenter: self, token: token, notificationName: notificationName)
     }
 }
+
+public class TransactionNotification {
+    
+    public let transactions: [TransactionProtocol]
+    
+    init(transactions: [TransactionProtocol]) {
+        self.transactions = transactions
+    }
+    
+    convenience init?(notification: Notification) {
+        guard let data = notification.userInfo?["transactions"] as? [TransactionProtocol] else {
+            return nil
+        }
+        self.init(transactions: data)
+    }
+    
+    var notification: Notification {
+        return Notification(name: Notification.Name.transactionsDidChange,
+                            object: nil,
+                            userInfo: ["transactions": transactions])
+    }
+}
+
+public protocol TransactionNotifications {
+    func postTransactionsDidChange(notification: TransactionNotification)
+    func observeTransactionsDidChange(callback: @escaping (TransactionNotification) -> Void) -> ObservationToken
+}
+
+extension NotificationCenter: TransactionNotifications {
+    
+    public func postTransactionsDidChange(notification: TransactionNotification) {
+        post(notification.notification)
+    }
+    
+    public func observeTransactionsDidChange(callback: @escaping (TransactionNotification) -> Void) -> ObservationToken {
+        let notificationName = Notification.Name.transactionsDidChange
+        let token = addObserver(forName: notificationName, object: nil, queue: nil) { (note) in
+            guard let transactionNotification = TransactionNotification(notification: note) else { return }
+            callback(transactionNotification)
+        }
+        return ObservationToken(notificationCenter: self, token: token, notificationName: notificationName)
+    }
+}
+
+extension Notification.Name {
+    
+    static var transactionsDidChange: Notification.Name {
+        return Notification.Name("com.money.saver.app.transactionsDidChange")
+    }
+}
