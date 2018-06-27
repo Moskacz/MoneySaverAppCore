e0cde8d615877c81d48c69c8e952a43314de7414
Michał Moskała <moskala.michal@gmail.com>
2018-06-27 19:13:54 +0200

    BudgetViewModelImpl class

--- Sources/Budget/BudgetViewModel.swift
+++ Sources/Budget/BudgetViewModel.swift
@@ -14,5 +14,34 @@ protocol BudgetViewModelDelegate: class {
 }
 
 protocol BudgetViewModel {
-    var delegate: BudgetViewModelDelegate { get set }
+    var delegate: BudgetViewModelDelegate? { get set }
+}
+
+public final class BudgetViewModelImpl: BudgetViewModel {
+    
+    weak var delegate: BudgetViewModelDelegate?
+    
+    private let budgetRepository: BudgetRepository
+    private let transactionsRepository: TransactionsRepository
+    private let chartsDataProcessor: BudgetChartsDataProcessor
+    private var observationToken: ObservationToken?
+    
+    public init(budgetRepository: BudgetRepository,
+                transactionsRepository: TransactionsRepository,
+                chartsDataProcessor: BudgetChartsDataProcessor) {
+        self.budgetRepository = budgetRepository
+        self.transactionsRepository = transactionsRepository
+        self.chartsDataProcessor = chartsDataProcessor
+        registerForNotifications()
+    }
+    
+    private func registerForNotifications() {
+        observationToken = transactionsRepository.observeTransactionsChanged(callback: { (transactions) in
+            
+        })
+    }
+    
+    private func updateBudgetData(transactions: [TransactionProtocol], budget: Decimal) {
+        
+    }
 }
