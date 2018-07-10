be01103e75a23339544cb269b4aa590edfbd72da
Michał Moskała <moskala.michal@gmail.com>
2018-07-10 20:02:19 +0200

    Added TransactionsSummaryViewModel protocol

---
+++ Sources/Summary/TransactionsSummaryViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionsSummaryViewModel.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 10.07.2018.
+//
+
+import Foundation
+
+public protocol TransactionsSummaryViewModelDelegate: class {
+    func transactionsSummaryDidUpdateValues(viewModel: TransactionsSummaryViewModel)
+}
+
+public protocol TransactionsSummaryViewModel {
+    var totalAmountText: String { get }
+    var expensesAmountText: String { get }
+    var incomesAmountText: String { get }
+    var dateRangeButtonText: String { get }
+    func set(dateRange: DateRange)
+}
