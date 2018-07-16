a613c34f8bf28251f32ef0df0b7ac0e2a35d5c59
Michał Moskała <moskala.michal@gmail.com>
2018-07-16 19:07:15 +0200

    Added TransactionsResultController

--- Sources/CoreData/repositories/CoreDataTransactionsRepository.swift
+++ Sources/CoreData/repositories/CoreDataTransactionsRepository.swift
@@ -28,18 +28,18 @@ internal class CoreDataTransactionsRepository: TransactionsRepository {
     }
     
     internal var allTransactionsResultController: ResultsController<TransactionProtocol> {
-//        let request: NSFetchRequest<TransactionManagedObject> = TransactionManagedObject.fetchRequest()
-//        request.includesPropertyValues = true
-//        request.fetchBatchSize = 20
-//
-//        request.sortDescriptors = [NSSortDescriptor(key: TransactionManagedObject.KeyPath.dayOfEra.rawValue, ascending: false),
-//                                   NSSortDescriptor(key: TransactionManagedObject.KeyPath.timeInterval.rawValue, ascending: false)]
-//
-//        return NSFetchedResultsController(fetchRequest: request,
-//                                          managedObjectContext: context,
-//                                          sectionNameKeyPath: TransactionManagedObject.KeyPath.dayOfEra.rawValue,
-//                                          cacheName: nil)
-        fatalError()
+        let request: NSFetchRequest<TransactionManagedObject> = TransactionManagedObject.fetchRequest()
+        request.includesPropertyValues = true
+        request.fetchBatchSize = 20
+
+        request.sortDescriptors = [NSSortDescriptor(key: TransactionManagedObject.KeyPath.dayOfEra.rawValue, ascending: false),
+                                   NSSortDescriptor(key: TransactionManagedObject.KeyPath.timeInterval.rawValue, ascending: false)]
+
+        let frc = NSFetchedResultsController(fetchRequest: request,
+                                          managedObjectContext: context,
+                                          sectionNameKeyPath: TransactionManagedObject.KeyPath.dayOfEra.rawValue,
+                                          cacheName: nil)
+        return TransactionResultsController(frc: frc)
     }
     
     internal func addTransaction(data: TransactionData, category: TransactionCategoryManagedObject) {
@@ -83,5 +83,35 @@ internal class CoreDataTransactionsRepository: TransactionsRepository {
         } catch {
             
         }
+    }
+}
+
+private class TransactionResultsController: ResultsController<TransactionProtocol> {
+    
+    private let controller: CoreDataResultsController<TransactionManagedObject>
+    
+    init(frc: NSFetchedResultsController<TransactionManagedObject>) {
+        self.controller = CoreDataResultsController(frc: frc)
+    }
+    
+    override var delegate: ResultsControllerDelegate? {
+        get { return controller.delegate }
+        set { controller.delegate = newValue }
+    }
+    
+    override func loadData() throws {
+        try controller.loadData()
+    }
+    
+    override func object(at indexPath: IndexPath) -> TransactionProtocol {
+        return controller.object(at: indexPath)
+    }
+    
+    override func objects(in section: Int) -> [TransactionProtocol]? {
+        return controller.objects(in: section)
+    }
+    
+    override var sectionsCount: Int {
+        return controller.sectionsCount
     }
 }
