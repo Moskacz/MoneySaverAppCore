86bd2c44c8b6142636a17b24920e483a075203b5
Michał Moskała <moskala.michal@gmail.com>
2018-07-22 20:49:03 +0200

    Added Factory protocol

---
+++ Sources/Factory.swift
@@ -0,0 +1,15 @@
+//
+//  Factory.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 22.07.2018.
+//
+
+import Foundation
+
+public protocol Factory {
+    func makeTransactionsListViewModel() -> TransactionsListViewModel
+    func makeTransactionsSummaryViewModel() -> TransactionsSummaryViewModel
+    func makeBudgetViewModel() -> BudgetViewModel
+    func makeStatsViewModel() -> StatsViewModel
+}
