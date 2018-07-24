ec2f67d0d0155d6777434c987b8cadb0680c1489
Michał Moskała <moskala.michal@gmail.com>
2018-07-24 22:19:44 +0200

    Added SetupBudgetViewModel

---
+++ Sources/Budget/SetupBudget/SetupBudgetViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SetupBudgetViewModel.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 24.07.2018.
+//
+
+import Foundation
+
+public protocol SetupBudgetViewModel {
+    func save(budget: Double)
+}
