539256b95dc06b3b85548d56ee0bfdc22367155f
Michał Moskała <moskala.michal@gmail.com>
2018-09-16 16:07:58 +0200

    New protocols

---
+++ Sources/TransactionCategories/TransactionCategoryCollectionCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionCategoryCollectionCoordinator.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 16.09.2018.
+//
+
+import Foundation
+
+public protocol TransactionCategoryCollectionCoordinator {
+    var display: TransactionCategoryCollectionDisplaying { set get }
+}
---
+++ Sources/TransactionCategories/TransactionCategoryCollectionDisplaying.swift
@@ -0,0 +1,11 @@
+//
+//  TransactionCategoryCollectionDisplaying.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 16.09.2018.
+//
+
+import Foundation
+import MMFoundation
+
+public protocol TransactionCategoryCollectionDisplaying: ResultsControllerDelegate {}
