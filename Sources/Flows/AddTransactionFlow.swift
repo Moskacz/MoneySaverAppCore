e689a80221c5a76f313c5e4d2521bbf1c11c5c0c
Michał Moskała <moskala.michal@gmail.com>
2018-09-19 19:11:15 +0200

    Added AddTransactionFlow

---
+++ Sources/Flows/AddTransactionFlow.swift
@@ -0,0 +1,14 @@
+//
+//  AddTransactionFlow.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 19.09.2018.
+//
+
+import Foundation
+
+public protocol AddTransactionFlow {
+    var transactionData: TransactionData? { get set }
+    var category: TransactionCategoryProtocol { get set }
+}
+
