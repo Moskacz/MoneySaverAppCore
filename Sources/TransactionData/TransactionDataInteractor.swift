d04a25e0673fd8c27b43b67ba221b7992809f46b
Michał Moskała <moskala.michal@gmail.com>
2018-11-24 21:36:32 +0100

    Add TransactionDataInteractor

---
+++ Sources/TransactionData/TransactionDataInteractor.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionDataInteractor.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 24/11/2018.
+//
+
+import Foundation
+
+protocol TransactionDataInteractorProtocol {
+    
+}
