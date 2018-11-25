58b33a481c204d26c4c9ee2e206353b158e6cc9b
Michał Moskała <moskala.michal@gmail.com>
2018-11-25 15:11:50 +0100

    Remove unused files

--- Sources/Factory.swift
+++ Sources/Factory.swift
@@ -9,5 +9,5 @@ import Foundation
 
 public protocol Factory {
     func makeTransactionsSummaryCoordinator() -> TransactionsSummaryPresenterProtocol
-    func makeStatsViewModel() -> StatsViewModel
+    
 }
