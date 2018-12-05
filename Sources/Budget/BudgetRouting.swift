9d9d6745448b433dcda41fcf3e1cd241869aba3d
Michał Moskała <moskala.michal@gmail.com>
2018-12-05 21:39:46 +0100

    Add SetupBudgetPresenter class

--- Sources/Budget/BudgetRouting.swift
+++ Sources/Budget/BudgetRouting.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
-public protocol BudgetRoutingProtocol {
+public protocol BudgetRoutingProtocol: class {
     func presentBudgetAmountEditor()
+    func dismissBudgetAmountEditor()
 }
--- Sources/Budget/SetupBudget/SetupBudgetPresenter.swift
+++ Sources/Budget/SetupBudget/SetupBudgetPresenter.swift
@@ -18,5 +18,31 @@ public enum SetupBudgetError {
 
 internal class SetupBudgetPresenter {
     
+    private let interactor: SetupBudgetInteractorProtocol
+    private unowned let userInterface: SetupBudgetUI
+    private unowned let router: BudgetRoutingProtocol
     
+    internal init(interactor: SetupBudgetInteractorProtocol,
+                  userInterface: SetupBudgetUI,
+                  router: BudgetRoutingProtocol) {
+        self.interactor = interactor
+        self.userInterface = userInterface
+        self.router = router
+    }
+}
+
+extension SetupBudgetPresenter: SetupBudgetPresenterProtocol {
+    
+    func closeViewClicked() {
+        router.dismissBudgetAmountEditor()
+    }
+    
+    func save(budget: String?) {
+        guard let text = budget, !text.isEmpty, let value = Decimal(string: text), !value.isNaN else {
+            userInterface.display(error: .invalidAmount)
+            return
+        }
+        
+        interactor.save(budget: value)
+    }
 }
