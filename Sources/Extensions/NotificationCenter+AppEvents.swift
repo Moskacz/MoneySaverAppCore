f45144b94faec2f6ee0baeae6326ea5bacc45dcb
Michał Moskała <moskala.michal@gmail.com>
2018-12-13 22:11:26 +0100

    Implement observeAppWillTerminateEvent method for iOS and macOS

--- Sources/Extensions/NotificationCenter+AppEvents.swift
+++ Sources/Extensions/NotificationCenter+AppEvents.swift
@@ -5,9 +5,34 @@
 //  Created by Michal Moskala on 13/12/2018.
 //
 
-import Foundation
+#if os(OSX)
+import AppKit
+#elseif os(iOS)
+import UIKit
+#endif
 
 internal protocol AppEventsNotificationCenter {
     func observeAppWillEnterBackgroundEvent(callback: @escaping () -> ())
-    func observeAppWillTerminateEvent(callback: @escaping () -> ())
+    func observeAppWillTerminateEvent(callback: @escaping () -> ()) -> ObservationToken
+}
+
+extension NotificationCenter: AppEventsNotificationCenter {
+    
+    func observeAppWillEnterBackgroundEvent(callback: @escaping () -> ()) {
+        
+    }
+    
+    func observeAppWillTerminateEvent(callback: @escaping () -> ()) -> ObservationToken {
+        let notificationName: NSNotification.Name
+        #if os(OSX)
+        notificationName = NSApplication.willTerminateNotification
+        #elseif os(iOS)
+        notificationName = UIApplication.willTerminateNotification
+        #endif
+        let token = self.addObserver(forName: notificationName, object: nil, queue: .main) { _ in
+            callback()
+        }
+        
+        return ObservationToken(notificationCenter: self, token: token, notificationName: notificationName)
+    }
 }
