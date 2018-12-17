59bd6e10ce80ac72d1a865dfb0b118c6c5912d03
Michał Moskała <moskala.michal@gmail.com>
2018-12-17 21:03:39 +0100

    Added presenter and interactor for transactions overview module

---
+++ Sources/TransactionsOverview/TransactionsOverviewInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionsOverviewInteractor.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 17/12/2018.
+//
+
+import Foundation
---
+++ Sources/TransactionsOverview/TransactionsOverviewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionsOverviewPresenter.swift
+//  MoneySaverAppCore
+//
+//  Created by Michal Moskala on 17/12/2018.
+//
+
+import Foundation
